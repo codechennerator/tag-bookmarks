b777b5d056d152823a5a7fa09e4e66c9fadfd694
Nathan Chen <chenl.nathan@gmail.com>
2018-07-30 14:36:44 -0700

    used chrome tabs to grab currentTab url

--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
 
 document.addEventListener("DOMContentLoaded", function(){
     let titleIn = document.querySelector('#title');
-
+    let urlHere = document.querySelector('#url');
     chrome.tabs.getSelected(function(tab){
         titleIn.value = tab.title;
+        urlHere.innerHTML = `URL: ${tab.url}`;
     });
 
 });
